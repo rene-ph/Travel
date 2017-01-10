6d9cedde3a9823a776d51b1b30e9c62e1fdfb9f1
Rene Hinojosa <rphs09@hotmail.com>
2017-01-10 01:32:34 -0600

    Added everything left

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,18 @@
+/**
+ * The way it works is if you have a css class with the name of flexbox
+ *  .flexbox .some-other-name { }
+ * it will detect the CLASS NAME but it doesnt mean in reality if it has a property or not inside the whole CSS.
+ * DO NOT USE THIS 
+*/
+var gulp = require('gulp'),
+modernizr = require('gulp-modernizr');
+
+gulp.task('modernizr', function() {
+ return  gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
+        .pipe(modernizr({
+          "options": [
+             "setClasses" 
+           ]
+        }))
+        .pipe(gulp.dest('./app/temp/scripts/'));
+});
